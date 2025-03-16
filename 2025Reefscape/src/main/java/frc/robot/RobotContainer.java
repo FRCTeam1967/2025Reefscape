7d1538ad0a263eabc324016454dbe3a731d74351
sana khan <sanakhantiwary@gmail.com>
2025-03-16 09:50:36 -0700

    added moving elevator slightly up after scoring coral but before moving pivot up to prevent getting caught -- UNTESTED
    Co-Authored-By: arushi-shah <149740330+arushi-shah@users.noreply.github.com>
    Co-Authored-By: SamanyaG <94148712+SamanyaG@users.noreply.github.com>

--- 2025Reefscape/src/main/java/frc/robot/RobotContainer.java
+++ 2025Reefscape/src/main/java/frc/robot/RobotContainer.java
@@ -399,8 +399,8 @@ public class RobotContainer {
             new RunCoralFastIntake(coralIntake, Constants.CoralIntake.HIGH).withTimeout(1),
             new ParallelCommandGroup(
                 new RunCoralFastIntake(coralIntake, Constants.CoralIntake.HIGH),
-                new MoveCoralPivot(coralPivot, Constants.CoralPivot.SAFE)
-            ).withTimeout(2))); //maybe change to 2.5
+                new MoveElevator(elevator, Constants.Elevator.EXTRA_HEIGHT, algaeMechanism)).withTimeout(2),
+            new MoveCoralPivot(coralPivot, Constants.CoralPivot.SAFE).withTimeout(2)));
 
         //LEFT BRANCH L2
         operatorController.cross().and(operatorController.L1()).whileTrue(new SequentialCommandGroup(
@@ -443,8 +443,8 @@ public class RobotContainer {
             new RunCoralFastIntake(coralIntake, Constants.CoralIntake.HIGH).withTimeout(1),
             new ParallelCommandGroup(
                 new RunCoralFastIntake(coralIntake, Constants.CoralIntake.HIGH),
-                new MoveCoralPivot(coralPivot, Constants.CoralPivot.SAFE)
-            ).withTimeout(2)) ); //maybe change to 2.5
+                new MoveElevator(elevator, Constants.Elevator.EXTRA_HEIGHT, algaeMechanism)).withTimeout(2),
+            new MoveCoralPivot(coralPivot, Constants.CoralPivot.SAFE).withTimeout(2)));
         
 
         //BARGE SCORING 
