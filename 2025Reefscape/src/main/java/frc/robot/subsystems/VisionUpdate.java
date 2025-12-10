9cd6050d0f744741d156597f02b59123de3f9edf
Janksters FRC Team 1967 <team1967@ndsj.org>
2025-12-09 16:30:05 -0800

    Revert "started implementing filtering methods (in progress)"
    This reverts commit 26ab04676797cae9bed78ad558dbfe791b26496b.

--- 2025Reefscape/src/main/java/frc/robot/subsystems/VisionUpdate.java
+++ 2025Reefscape/src/main/java/frc/robot/subsystems/VisionUpdate.java
@@ -6,33 +6,18 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.LimelightHelpers;
-import frc.robot.LimelightHelpers.LimelightResults;
-import frc.robot.LimelightHelpers.LimelightTarget_Detector;
-import frc.robot.LimelightHelpers.LimelightTarget_Fiducial;
-import frc.robot.LimelightHelpers.RawFiducial;
 import dev.doglog.DogLog;
-
-import java.lang.reflect.Array;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.BooleanSupplier;
-import java.util.stream.Collectors;
-
 import com.ctre.phoenix6.Utils;
-import edu.wpi.first.units.Units.*;
 
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
-import edu.wpi.first.math.numbers.N1;
-import edu.wpi.first.math.numbers.N3;
-import edu.wpi.first.math.*;
 import edu.wpi.first.networktables.IntegerPublisher;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructPublisher;
-// import edu.wpi.first.Math.Matrix;
 
 public class VisionUpdate extends SubsystemBase {
   private final CommandSwerveDrivetrain drivetrain;
@@ -43,23 +28,11 @@ public class VisionUpdate extends SubsystemBase {
   private long odometryDiscards = 0;
   IntegerPublisher updatePublisher;
   IntegerPublisher discardPublisher;
-  String camTitle;
-  LimelightResults latestVisionResult;
-  LimelightTarget_Detector detector;
-  BooleanSupplier isRed;
-  
-  private static final List<Integer> BLUE_SIDE_TAG_IDS = List.of(19, 20, 21, 22, 17, 18);
-  private static final List<Integer> RED_SIDE_TAG_IDS = List.of(6, 7, 8, 9, 10, 11);
 
-  private double maximumAllowedDistance = 15.0; // meters, beyond which readings are dropped, from 3501
-  
   /** Creates a new VisionUpdate. */
-  public VisionUpdate(CommandSwerveDrivetrain drivetrain, SwerveDrivePoseEstimator m_poseEstimator, BooleanSupplier isRed) {
+  public VisionUpdate(CommandSwerveDrivetrain drivetrain, SwerveDrivePoseEstimator m_poseEstimator) {
     this.drivetrain = drivetrain;
     this.m_poseEstimator = m_poseEstimator;
-    this.isRed = isRed;
-
-    camTitle = "limelight-santos";
 
     limelightPublisher = NetworkTableInstance.getDefault().getTable("limelight-santos").getStructTopic("Limelight Pose", Pose2d.struct).publish();
 
@@ -68,12 +41,8 @@ public class VisionUpdate extends SubsystemBase {
 
     updatePublisher = table.getIntegerTopic("acceptedUpdates").publish();
     discardPublisher = table.getIntegerTopic("rejectedUpdates").publish();
-
-    LimelightResults latestVisionResult = new LimelightResults();
-
-    LimelightTarget_Detector detector = new LimelightTarget_Detector();
   }
-  
+
   @Override
   public void periodic() {
     // Tell Limelight what our current orientation is
@@ -122,143 +91,6 @@ public class VisionUpdate extends SubsystemBase {
     }
 
     DogLog.log("VisionUpdate/acceptedUpdate", !doRejectUpdate);
-  }
-
-  //helper filtering method
-  public void addFilteredPose() {
-    SwerveDrivePoseEstimator selectedEstimator = m_poseEstimator;
-
-    if (latestVisionResult == null || latestVisionResult.botpose_tagcount == 0) {
-      DogLog.log("Vision/" + camTitle + "/HasTargets", false);
-      return;
-    }
-    
-    DogLog.log("Vision/" + camTitle + "/HasTargets", true);
-
-    double averageDistance = latestVisionResult.botpose_avgdist;
-
-    double minDistance = 
-        latestVisionResult.getTargets().stream()
-            .mapToDouble(t -> t.getBestCameraToTarget().getTranslation().getNorm())
-            .min()
-            .orElse(Double.NaN);
-
-    // Filter tags to only those on the active side
-    List<PhotonTrackedTarget> validTags =
-        latestVisionResult.getTargets().stream()
-            .filter(t -> isTagOnActiveSide(t.getFiducialId()))
-            .filter(t -> isNotChopped(t.getYaw()))
-            .collect(Collectors.toList());
-
-    DogLog.log("Vision/" + camTitle + "/numValidTags", validTags.size());
-
-    for (PhotonTrackedTarget tag : validTags) {
-      DogLog.log("Vision/" + camTitle + "/Area", tag.getArea());
-      DogLog.log("Vision/" + camTitle + "/Yaw", tag.getYaw());
-    }
-
-    DogLog.log("Vision/" + camTitle + "/allTagIds", allTagIds);
-
-    if (validTags.isEmpty()) {
-      DogLog.log("Vision/" + camTitle + "/ValidTags", false);
-      return;
-    }
-    DogLog.log("Vision/" + camTitle + "/ValidTags", true);
-
-    // Log all tags that haven't been thrown out
-    int tagCount = validTags.size();
-
-    // Compute effective metrics for solution
-    // Use camera→target distance from PV (avoids odometry dependence)
-
-    // nothing to do if rejected based on the minDistance or if no min dist has been found
-    if (Double.isNaN(minDistance) || minDistance > maximumAllowedDistance) {
-      DogLog.log("Vision/" + camTitle + "/ThrownOutDistance", true);
-      return;
-    }
-    DogLog.log("Vision/" + camTitle + "/ThrownOutDistance", false);
-
-    // find the current speed
-    double currentSpeed =
-        Math.hypot(
-            swerveDrive.getRobotSpeeds().vxMetersPerSecond,
-            swerveDrive.getRobotSpeeds().vyMetersPerSecond);
-    DogLog.log("Vision/" + camTitle + "/VisionDrivebaseSpeed", currentSpeed);
-
-    double nX =
-        computeNoiseXY(
-            baseNoiseX,
-            distanceExponentialCoefficientX,
-            distanceExponentialBaseX,
-            angleCoefficientX,
-            speedCoefficientX,
-            averageDistance,
-            currentSpeed,
-            tagCount);
-    double nY =
-        computeNoiseXY(
-            baseNoiseY,
-            distanceExponentialCoefficientY,
-            distanceExponentialBaseY,
-            angleCoefficientY,
-            speedCoefficientY,
-            averageDistance,
-            currentSpeed,
-            tagCount);
-    double nTH =
-        computeNoiseHeading(
-            baseNoiseTheta,
-            distanceCoefficientTheta,
-            angleCoefficientTheta,
-            speedCoefficientTheta,
-            averageDistance,
-            currentSpeed,
-            tagCount);
-
-    DogLog.log("Vision/" + camTitle + "/speed", currentSpeed);
-    DogLog.log("Vision/" + camTitle + "/nX", nX);
-    DogLog.log("Vision/" + camTitle + "/nY", nY);
-    DogLog.log("Vision/" + camTitle + "/nTH", nTH);
-    DogLog.log("Vision/" + camTitle + "/Pose", measuredPose);
-    DogLog.log("Vision/" + camTitle + "/averageDistance", averageDistance);
-
-    Matrix<N3, N1> noiseVector = VecBuilder.fill(nX, nY, nTH);
-    // Process locally (no cross-camera comparison)
-    processPoseEstimate(
-        measuredPose,
-        averageDistance,
-        currentSpeed,
-        tagCount,
-        latestVisionResult.getTimestampSeconds(),
-        noiseVector);
-  }
-
-  /** check if valid */
-  private boolean isValid(int fiducial, LimelightResults results) {
-    return isTagOnActiveSide(fiducial) && isTagNotSkew(results);
-  }
-
-  /** check if the tags seen are on active alliance side */
-  private boolean isTagOnActiveSide(int tagId) {
-    return isRed.getAsBoolean()
-        ? RED_SIDE_TAG_IDS.contains(tagId)
-        : BLUE_SIDE_TAG_IDS.contains(tagId);
-  }
-
-  /** get all tags */
-  private int[] getAllTags(LimelightResults result){
-    int[] tags = new int[(int) result.botpose_tagcount];
-    for (int i = 0; i < result.botpose_tagcount; i++) {
-      tags[i] = (int) result.targets_Fiducials[i].fiducialID;
-    }
-
-    return tags;
-  }
-
-  /** make sure yaw isn't too skewed */
-  private boolean isTagNotSkew(LimelightResults result){
-    double radians = Math.toRadians(60);
-    return (result.getBotPose3d_wpiBlue().getRotation().getAngle() > radians);
   }
 
     // //This method will be called once per scheduler run
